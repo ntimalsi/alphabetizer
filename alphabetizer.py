d738316f05670e8a5991161da0c18a6a699e9ff4
Nischal <nischal.timalsina@gmail.com>
2022-04-25 18:10:41 -0400

    improvised

--- alphabetizer.py
+++ alphabetizer.py
@@ -1,13 +1,32 @@
-tester = "3 Blind Mice"
+#!/usr/bin/env python3
 
-def alphabetizer(givenString):
-    sortString = sorted(givenString, key=str.lower)
+def alphabetizer(givenString: str) -> str:
+    """
+     Using Python's sorted function to sort the result. Thus, Time Complexity is O(nLogn) due to Tim Sort
+    Args:
+    givenString (str): The string to test
+
+    Returns:
+    finalString (str): The alphabetized string
+    """
+    sortString = sorted(givenString, key=str.lower) 
     finalString=""
+    if not givenString:
+        return ""
+
     for string in sortString:
         if not string.isalpha():
             continue
         finalString+= string
     return finalString
 
+if __name__ == '__main__':
+    try:
+        while True:
+            tester = input("Enter a string to alphabetize or press Ctrl+C to interrupt:")
+            print(alphabetizer(tester))
 
-print(alphabetizer(tester))
+    except (KeyboardInterrupt):
+        print("Interrupted")
+    finally:
+        print("\nClosed")
